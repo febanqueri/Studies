eebd8a1535b3ee0473d83d4d8e5144f6f8943019
febanqueri <56729735+febanqueri@users.noreply.github.com>
2020-06-05 22:51:53 -0300

    Exercicio 25

---
+++ Python - Word 1 {CURSO EM VIDEO}/exercicio_025.py
@@ -0,0 +1,4 @@
+#Procurando uma string dentro de outra:
+print('Crie um programa que leia o nome de uma pessoa e diga se ela tem "SILVA" no nome.')
+nome = str(input('Digite o seu nome completo: ')).strip()
+print('Seu nome tem Silva? {} .'.format('SILVA' in nome.upper()))
