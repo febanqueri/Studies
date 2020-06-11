74d0cda781d35b248b4067a488b08d2ec1a4ba1f
febanqueri <56729735+febanqueri@users.noreply.github.com>
2020-06-11 12:08:25 -0300

    Exercicios e aula 11

---
+++ Python - Word 1 {CURSO EM VIDEO}/aula_11.py
@@ -0,0 +1,13 @@
+#Cores no Terminal.
+# \033[0:33:43m
+# 0 none
+# 1 bold
+# 4 underline
+# 7 negative
+
+
+# Text : 30 (White) // 31 (Red) // (32 (Green) // 33 (Yellow) // 34 (Blue) // 35 (Purple) // 36 (Sky blue) // 37 (gray)
+# Back : 40 (White) // 41 (Red) // (42 (Green) // 43 (Yellow) // 44 (Blue) // 45 (Purple) // 46 (Sky blue) // 47 (gray)
+
+print('\033[31;43mOlá, mundo!\033[m')
+print('\033[33;44mMeu nome é Felipe!\033[m')
